e00e4844fc9e0440986811cbfd5d65c2be00af6a
Kuaipz <124898145+Kuaipz@users.noreply.github.com>
2023-04-18 00:59:19 +0800

    Update ricedroid.mk

--- ricedroid.mk
+++ ricedroid.mk
@@ -3,14 +3,9 @@ $(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
 $(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common Ricedroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 -include vendor/lineage/build/core/config.mk
--include vendor/lineage/build/core/apicheck.mk
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
@@ -23,19 +18,7 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 WITH_GMS := true
+RICE_MAINTAINER := Kuaipz
 
 TARGET_BUILD_GRAPHENEOS_CAMERA := true
-
-
-# Use CCache
-USE_CCACHE := true
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-	persist.sys.binary_xml=false \
-	dalvik.vm.dex2oat64.enabled=true \
-
-# Set Bootanimation at 720P
-TARGET_BOOT_ANIMATION_RES := 720
-
-# APN
-PRODUCT_PACKAGES += apns-conf.xml
+TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2 := true
